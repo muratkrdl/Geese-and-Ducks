8a602303f016203bffca45b57ba390dc98fb54a5
Murat KIRDAL <muratkirdal0101@gmail.com>
2025-07-30 15:26:28 +0300

    .

--- Assets/Scripts/HasanBugra/GameManager/EnemySpawn.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using UnityEngine;
-
-[Serializable]
-public class EnemySpawn
-{
-    public GameObject enemyPrefab;
-    [Range(0f, 100f)] public float spawnChance = 100f; 
-}
--- Assets/Scripts/Murat/Utilities/ConstUtilities.cs
+++
@@ -1,7 +0,0 @@
-namespace Murat.Utilities
-{
-    public static class ConstUtilities
-    {
-        public const string ENEMY_TAG = "Enemy";
-    }
-}

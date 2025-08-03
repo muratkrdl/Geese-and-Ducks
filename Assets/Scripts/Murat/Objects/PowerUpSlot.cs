a931dd5ba59ab00cfe8a6bc6496eaeab2e2aaaba
Murat KIRDAL <muratkirdal0101@gmail.com>
2025-08-03 12:15:48 +0300

    PoweUpgrade Art Implemented

--- Assets/Scripts/Murat/Objects/PowerUpSlot.cs
+++ Assets/Scripts/Murat/Objects/PowerUpSlot.cs
@@ -1,5 +1,7 @@
 using DG.Tweening;
 using Murat.Data.UnityObject;
+using Murat.Data.UnityObject.CDS;
+using Murat.Data.ValueObject;
 using Murat.Managers;
 using TMPro;
 using UnityEngine;
@@ -9,9 +11,6 @@ namespace Murat.Objects
 {
     public class PowerUpSlot : MonoBehaviour
     {
-        [SerializeField] private float animDuration;
-        [SerializeField] private Ease animEase;
-        
         [SerializeField] private GameObject descriptionPanel;
 
         [SerializeField] private string playerPrefCode;
@@ -19,60 +18,59 @@ namespace Murat.Objects
         [SerializeField] private int code;
 
         [SerializeField] private Image iconImage;
-        [SerializeField] private TextMeshProUGUI SkillName;
-        [SerializeField] private TextMeshProUGUI Description;
-        [SerializeField] private TextMeshProUGUI Cost;
+        [SerializeField] private TextMeshProUGUI description;
+        [SerializeField] private TextMeshProUGUI cost;
     
         [SerializeField] private Image[] levelImages;
     
-        private PowerUpSO powerUpSO;
-    
-        private string playerPrefLevelCode;
+        private PowerUpSO _powerUpSo;
+        private PanelPopUpData _data;
+        private string _playerPrefLevelCode;
     
         private void Start() 
         {
-            playerPrefLevelCode = playerPrefCode + "Level";
+            _playerPrefLevelCode = playerPrefCode + "Level";
+            _data = Resources.Load<CD_PANEL>("Data/CDS/CD_PANEL").PanelPopUpData;
+            _powerUpSo = PowerUpSOKeeper.Instance.GetPermanentSkillSoByCode(code, PlayerPrefs.GetInt(_playerPrefLevelCode));
             GetComponent<Button>().onClick.AddListener(OnClickEvent);
-            powerUpSO = PowerUpSOKeeper.Instance.GetPermanentSkillSoByCode(code, PlayerPrefs.GetInt(playerPrefLevelCode));
             SetValues();
         }
         
         private void OnClickEvent()
         {
-            if(powerUpSO.Full) return;
+            if(_powerUpSo.Full) return;
 
             SetDescriptionPanel(descriptionPanel.transform.localScale != Vector3.one);
         }
 
         private void SetValues()
         {
-            iconImage.sprite = powerUpSO.SkillIcon;
-            SkillName.text = powerUpSO.SkillName;
+            iconImage.sprite = _powerUpSo.SkillIcon;
     
             for(int i = 0; i < levelImages.Length; i++)
             {
-                if(i < powerUpSO.Level)
+                if(i < _powerUpSo.Level)
                     levelImages[i].color = new(0, 0.64f, 0, 1);
             }
     
-            if(powerUpSO.Full)
+            if(_powerUpSo.Full)
             {
                 SetDescriptionPanel(false);
                 return;
             }
-            PowerUpSO nextSkillSo = PowerUpSOKeeper.Instance.GetPermanentSkillSoByCode(code, PlayerPrefs.GetInt(playerPrefLevelCode) + 1);;
-            Description.text = nextSkillSo.Description;
-            Cost.text = nextSkillSo.Cost.ToString();
+            PowerUpSO nextSkillSo = PowerUpSOKeeper.Instance.GetPermanentSkillSoByCode(code, PlayerPrefs.GetInt(_playerPrefLevelCode) + 1);;
+            description.text = nextSkillSo.Description;
+            cost.text = nextSkillSo.Cost.ToString();
         }
 
         public void Buy()
         {
             if( /* Bank.Money*/ 999 >=
-               PowerUpSOKeeper.Instance.GetPermanentSkillSoByCode(code, PlayerPrefs.GetInt(playerPrefLevelCode) + 1).Cost) 
+               PowerUpSOKeeper.Instance.GetPermanentSkillSoByCode(code, PlayerPrefs.GetInt(_playerPrefLevelCode) + 1).Cost) 
             {
-                powerUpSO = PowerUpSOKeeper.Instance.GetPermanentSkillSoByCode(code, PlayerPrefs.GetInt(playerPrefLevelCode) + 1);
+                _powerUpSo = PowerUpSOKeeper.Instance.GetPermanentSkillSoByCode(code, PlayerPrefs.GetInt(_playerPrefLevelCode) + 1);
                 // Decrease Cost From Bank
-                PlayerPrefs.SetInt(playerPrefLevelCode, powerUpSO.Level);
+                PlayerPrefs.SetInt(_playerPrefLevelCode, _powerUpSo.Level);
                 // Update Money Text
             }
             
@@ -83,7 +81,7 @@ namespace Murat.Objects
         private void SetDescriptionPanel(bool value)
         {
             float targetValue = value ? 1f : 0f;
-            descriptionPanel.transform.DOScale(targetValue, animDuration).SetEase(animEase);
+            descriptionPanel.transform.DOScale(targetValue, _data.AnimationDuration).SetEase(_data.AnimationEase);
         }
     }
 }

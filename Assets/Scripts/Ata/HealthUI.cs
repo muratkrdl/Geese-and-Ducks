b2b0df229091792715b304ce5904d2516f067156
Hasan Buğra Duru <hasanbugraduru@hotmail.com>
2025-08-18 17:49:21 +0300

    Fix

--- Assets/Scripts/Ata/HealthUI.cs
+++ Assets/Scripts/Ata/HealthUI.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 public class HealthUI : MonoBehaviour
 {
     [SerializeField] private HeartOfLine heartOfLine; 
-    [SerializeField] private TextMeshProUGUI healthText; 
+    [SerializeField] private TextMeshProUGUI healthText;
 
+
+    private void Start()
+    {
+        heartOfLine = FindAnyObjectByType<HeartOfLine>();
+    }
     void Update()
     {
         if (heartOfLine != null && healthText != null)
